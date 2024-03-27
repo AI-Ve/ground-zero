01efcec87bdebfb8f75987454b4170a1cd768cc0
Alexandru Veltan <48562562+AI-Ve@users.noreply.github.com>
2024-03-27 12:13:43 +0200

    minor changes

--- snake/src/main.py
+++ snake/src/main.py
@@ -1,14 +1,38 @@
-import snake
 import curses
+from random import randint
+from typing import List
+
+
+def food(snake: List, sh: int, sw: int) -> List:
+    position = None
+    while position is None:
+        position = [randint(1, sh - 1), randint(1, sw - 1)]
+        if position in snake:
+            position = None
+    return position
 
 
 def c_main(scr: "curses._CursesWindow") -> int:
+    curses.curs_set(0)
+
     scr.clear()
 
+    window_height, window_length = scr.getmaxyx()
+    name = ""
 
-def main() -> int:
-    return curses.wrapper(c_main)
+    while True:
+        new_food = food([[1, 1], [1, 2], [1, 3]], window_height, window_length)
+        scr.addstr(new_food[0], new_food[1], "*")
+        name = scr.get_wch()
+
+        if name == "q":
+            break
+
+        scr.addstr(new_food[0], new_food[1], "")
+
+    return 0
 
 
 if __name__ == "__main__":
-    exit(main())
+    curses.wrapper(c_main)
+    SystemExit(0)
